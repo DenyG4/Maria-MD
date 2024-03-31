d8a3268bf811d87dca47da6492c83319947b3479
DenyG4 <165566881+DenyG4@users.noreply.github.com>
2024-03-31 15:27:59 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['263788249164']
+global.ownername = "warounamuruthi@gmail.com"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
